89355c25ba58846333a3e38357f327384e03c1a2
piyalish <55231406+piyalish@users.noreply.github.com>
2025-03-16 14:57:57 +0000

    Deploying to gh-pages from @ piyalish/piyalish.github.io@8e7be86c337c1b47ca63b312e7923d5b1f7fa9e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,7 +490,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73.%70%69%79%61%6C%69%31%36@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
