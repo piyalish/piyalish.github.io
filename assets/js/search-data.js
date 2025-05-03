db00dfed43a289c5aa2d9dc2f1af3fe66ab2630e
piyalish <55231406+piyalish@users.noreply.github.com>
2025-05-03 17:44:58 +0000

    Deploying to gh-pages from @ piyalish/piyalish.github.io@d11f76c23b2d604e1dec4786874630d1208e38da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,7 +412,7 @@ ninja.data = [{
               window.location.href = "/projects/1_project/";
             },},{id: "projects-alma-perseus-polarization-survey-alpps",
           title: 'ALMA Perseus Polarization Survey (ALPPS)',
-          description: "(PI - Chat Hull)",
+          description: "PI - Chat Hull",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
